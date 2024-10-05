aadd32b40c317f28942dbe24a2b0ff6bc310b08a
anhnhph33991 <hoang838604@gmail.com>
2024-10-06 03:56:07 +0700

    feat: buoi 4/middleware/authentication

--- routes/admin.php
+++ routes/admin.php
@@ -2,7 +2,14 @@
 
 use App\Http\Controllers\Admin\DashBoardController;
 use App\Http\Controllers\EmployeeController;
+use App\Http\Middleware\Authenticate;
+use App\Http\Middleware\FlagMiddleware;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [DashBoardController::class, 'index'])->name('dashboard');
-Route::resource('employees', EmployeeController::class);
+
+Route::middleware(['auth', FlagMiddleware::class])->group(function () {
+    Route::get('/', [DashBoardController::class, 'index'])->name('dashboard');
+    Route::resource('employees', EmployeeController::class);
+
+    Route::get('/luxchill', [DashBoardController::class, 'index'])->withoutMiddleware('auth');
+});
--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,12 @@
 
 use App\Http\Controllers\Client\HomeController;
 use Illuminate\Database\Query\Builder;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index']);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
